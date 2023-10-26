1f138be8c1d78c145483f77ae4bc0801de16148b
rickyeeez <wardhanaricky30@gmail.com>
2023-10-26 07:39:55 +0700

    first commit

---
+++ src/Page/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from '../components/Hero';
+
+const About = () => {
+  return (
+    <div>
+      <Hero />
+      <h2>About Us Page</h2>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Page/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from '../components/Hero';
+
+const Contact = () => {
+  return (
+    <div>
+      <Hero />
+      <h2>Contact Page</h2>
+    </div>
+  );
+};
+
+export default Contact;
